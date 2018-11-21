8e19d9764e30ebb6a80ac335d987d07624255334
Antoinette Watson <toniwats@gmail.com>
2018-11-21 09:58:53 -0500

    Expanded test results for MAGI Children HK1

--- rules/MAGI-Children-HK1_test.sh
+++ rules/MAGI-Children-HK1_test.sh
@@ -3,6 +3,7 @@ set -eux
 
 # Initialize table to test values
 sqlite3 core.db <<-EOF
+-- Clearing and reseting tables for test 
 DROP TABLE IF EXISTS CaseID;
 DROP TABLE IF EXISTS Case_Determinations;
 DROP TABLE IF EXISTS Determinations;
@@ -15,6 +16,22 @@ INSERT INTO Case_Determinations ( 'auto', 'CaseID', 'DeterminationID' ) VALUES (
 -- Our second case
 INSERT INTO CaseID ( 'CaseID', 'Age', 'FPL', 'Gender', 'Pregnant', 'ComprehensiveInsurance', 'Caretaker') VALUES (40, 4.7, 300, 'Male', 0, 0, 1);
 INSERT INTO Case_Determinations ( 'auto', 'CaseID', 'DeterminationID' ) VALUES (3, 40, 12);
+-- Our third case
+INSERT INTO CaseID ( 'CaseID', 'Age', 'FPL', 'Gender', 'Pregnant', 'ComprehensiveInsurance', 'Caretaker') VALUES (50, 54.5, 95, 'Female', 0, 0, 1);
+INSERT INTO Case_Determinations ( 'auto', 'CaseID', 'DeterminationID' ) VALUES (1, 41, 12);
+-- Our fourth case
+INSERT INTO CaseID ( 'CaseID', 'Age', 'FPL', 'Gender', 'Pregnant', 'ComprehensiveInsurance', 'Caretaker') VALUES (51, 21.7, 87, 'Male', 0, 0, 1);
+INSERT INTO Case_Determinations ( 'auto', 'CaseID', 'DeterminationID' ) VALUES (5, 42, 12);
+-- Our fifth case
+INSERT INTO CaseID ( 'CaseID', 'Age', 'FPL', 'Gender', 'Pregnant', 'ComprehensiveInsurance', 'Caretaker') VALUES (20, 30.7, 30, 'Male', 0, 0, 1);
+INSERT INTO Case_Determinations ( 'auto', 'CaseID', 'DeterminationID' ) VALUES (6, 44, 12);
+-- Our sixth case
+INSERT INTO CaseID ( 'CaseID', 'Age', 'FPL', 'Gender', 'Pregnant', 'ComprehensiveInsurance', 'Caretaker') VALUES (00, 0.0, 00, 'Female', 0, 0, 1);
+INSERT INTO Case_Determinations ( 'auto', 'CaseID', 'DeterminationID' ) VALUES (7, 45, 12);
+--Our seventh 
+INSERT INTO CaseID ( 'CaseID', 'Age', 'FPL', 'Gender', 'Pregnant', 'ComprehensiveInsurance', 'Caretaker') VALUES (44, 4.4, 244, 'Male', 0, 0, 1);
+INSERT INTO Case_Determinations ( 'auto', 'CaseID', 'DeterminationID' ) VALUES (9, 35, 12);
+-- Final wrap 
 INSERT INTO Determinations ( 'DeterminationID', 'executable', 'Active', 'DeterminationText' ) VALUES (12, "rules/MAGI-Children-HK1.py", 1, "MAGI-Children-HK1");
 .quit
 EOF
@@ -23,6 +40,11 @@ EOF
 # Run with required case info
 ./rules/MAGI-Children-HK1.py 2 42
 ./rules/MAGI-Children-HK1.py 3 40
+./rules/MAGI-Children-HK1.py 1 50
+./rules/MAGI-Children-HK1.py 5 51
+./rules/MAGI-Children-HK1.py 6 20
+./rules/MAGI-Children-HK1.py 7 00
+./rules/MAGI-Children-HK1.py 9 44
 
 # Get results to prove success
 {

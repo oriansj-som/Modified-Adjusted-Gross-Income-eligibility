5fd63bdf716d39f13fa573c7c7dc37954c3937e4
Antoinette Watson <toniwats@gmail.com>
2018-12-20 11:22:14 -0500

    Age and FPL updates

--- rules/MAGI-Children-HK1_test.sh
+++ rules/MAGI-Children-HK1_test.sh
@@ -6,33 +6,32 @@ sqlite3 core.db <<-EOF
 -- Clearing and reseting tables for test 
 DROP TABLE IF EXISTS CaseID;
 DROP TABLE IF EXISTS Case_Determinations;
-DROP TABLE IF EXISTS Determinations;
+
 CREATE TABLE 'CaseID' ( 'CaseID' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 'Age' INTEGER NOT NULL, 'FPL' INTEGER NOT NULL, 'Gender' TEXT NOT NULL, 'Pregnant' INTEGER NOT NULL, 'ComprehensiveInsurance' INTEGER NOT NULL, 'Caretaker' INTEGER NOT NULL );
-CREATE TABLE 'Case_Determinations' ( 'auto' INTEGER PRIMARY KEY AUTOINCREMENT, 'CaseID' INTEGER NOT NULL, 'DeterminationID' INTEGER NOT NULL, 'Result' INTEGER, 'Datetime' INTEGER, 'async' INTEGER, 'Flag' TEXT );
-CREATE TABLE 'Determinations' ( 'DeterminationID' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 'executable' TEXT NOT NULL, 'Active' INTEGER NOT NULL, 'DeterminationText' TEXT NOT NULL );
+CREATE TABLE 'Case_Determinations' ( 'auto' INTEGER PRIMARY KEY AUTOINCREMENT, 'CaseID' INTEGER NOT NULL, 'Result' INTEGER, 'Datetime' INTEGER, 'async' INTEGER, 'Flag' TEXT );
+
 -- Our first case
 INSERT INTO CaseID ( 'CaseID', 'Age', 'FPL', 'Gender', 'Pregnant', 'ComprehensiveInsurance', 'Caretaker') VALUES (42, 64.0, 22, 'Female', 0, 0, 0);
-INSERT INTO Case_Determinations ( 'auto', 'CaseID', 'DeterminationID' ) VALUES (2, 2, 12);
+INSERT INTO Case_Determinations ( 'auto', 'CaseID') VALUES (2, 2);
 -- Our second case
 INSERT INTO CaseID ( 'CaseID', 'Age', 'FPL', 'Gender', 'Pregnant', 'ComprehensiveInsurance', 'Caretaker') VALUES (40, 4.7, 300, 'Male', 0, 0, 1);
-INSERT INTO Case_Determinations ( 'auto', 'CaseID', 'DeterminationID' ) VALUES (3, 40, 12);
+INSERT INTO Case_Determinations ( 'auto', 'CaseID') VALUES (3, 40);
 -- Our third case
 INSERT INTO CaseID ( 'CaseID', 'Age', 'FPL', 'Gender', 'Pregnant', 'ComprehensiveInsurance', 'Caretaker') VALUES (50, 54.5, 95, 'Female', 0, 0, 1);
-INSERT INTO Case_Determinations ( 'auto', 'CaseID', 'DeterminationID' ) VALUES (1, 41, 12);
+INSERT INTO Case_Determinations ( 'auto', 'CaseID') VALUES (1, 41);
 -- Our fourth case
 INSERT INTO CaseID ( 'CaseID', 'Age', 'FPL', 'Gender', 'Pregnant', 'ComprehensiveInsurance', 'Caretaker') VALUES (51, 21.7, 87, 'Male', 0, 0, 1);
-INSERT INTO Case_Determinations ( 'auto', 'CaseID', 'DeterminationID' ) VALUES (5, 42, 12);
+INSERT INTO Case_Determinations ( 'auto', 'CaseID') VALUES (5, 42);
 -- Our fifth case
 INSERT INTO CaseID ( 'CaseID', 'Age', 'FPL', 'Gender', 'Pregnant', 'ComprehensiveInsurance', 'Caretaker') VALUES (20, 30.7, 30, 'Male', 0, 0, 1);
-INSERT INTO Case_Determinations ( 'auto', 'CaseID', 'DeterminationID' ) VALUES (6, 44, 12);
+INSERT INTO Case_Determinations ( 'auto', 'CaseID') VALUES (6, 44);
 -- Our sixth case
 INSERT INTO CaseID ( 'CaseID', 'Age', 'FPL', 'Gender', 'Pregnant', 'ComprehensiveInsurance', 'Caretaker') VALUES (00, 0.0, 00, 'Female', 0, 0, 1);
-INSERT INTO Case_Determinations ( 'auto', 'CaseID', 'DeterminationID' ) VALUES (7, 45, 12);
+INSERT INTO Case_Determinations ( 'auto', 'CaseID') VALUES (7, 45);
 --Our seventh 
 INSERT INTO CaseID ( 'CaseID', 'Age', 'FPL', 'Gender', 'Pregnant', 'ComprehensiveInsurance', 'Caretaker') VALUES (44, 4.4, 244, 'Male', 0, 0, 1);
-INSERT INTO Case_Determinations ( 'auto', 'CaseID', 'DeterminationID' ) VALUES (9, 35, 12);
+INSERT INTO Case_Determinations ( 'auto', 'CaseID') VALUES (9, 35);
 -- Final wrap 
-INSERT INTO Determinations ( 'DeterminationID', 'executable', 'Active', 'DeterminationText' ) VALUES (12, "rules/MAGI-Children-HK1.py", 1, "MAGI-Children-HK1");
 .quit
 EOF
 
@@ -50,7 +49,7 @@ EOF
 {
 sqlite3 core.db <<-EOF
 .separator "\t"
-SELECT CaseID, DeterminationID, Result, Datetime, async, Flag FROM Case_Determinations;
+SELECT CaseID,  Result, Flag FROM Case_Determinations;
 .quit
 EOF
 } >| rules/results/MAGI-Children-HK1-result
---
+++ rules/MAGI-Children-HKE_test.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+set -eux
+
+# Initialize table to test values
+sqlite3 core.db <<-EOF
+-- Clearing and reseting tables for test 
+DROP TABLE IF EXISTS CaseID;
+DROP TABLE IF EXISTS Case_Determinations;
+
+CREATE TABLE 'CaseID' ( 'CaseID' INTEGER, 'Age','FPL'  INTEGER, 'Gender', 'Pregnant', 'ComprehensiveInsurance', 'Caretaker'); 
+CREATE TABLE 'Case_Determinations' ( auto INTEGER, 'CaseID' INTEGER,  'Result'  INTEGER, 'Flag' TEXT);
+--our first case
+INSERT INTO CaseID ( 'CaseID', 'Age', 'FPL') VALUES (41, 41, 108);
+INSERT INTO Case_Determinations ( 'auto', 'CaseID') VALUES (6, 41);
+--our second case
+INSERT INTO CaseID ( 'CaseID', 'Age', 'FPL') VALUES (51, 61, 108);
+INSERT INTO Case_Determinations ( 'auto', 'CaseID') VALUES (7, 51);
+--our third case
+INSERT INTO CaseID ( 'CaseID', 'Age', 'FPL') VALUES (52, 62, 107);
+INSERT INTO Case_Determinations ( 'auto', 'CaseID') VALUES (8, 52);
+--our fourth case
+INSERT INTO CaseID ( 'CaseID', 'Age', 'FPL') VALUES (53, 63, 106);
+INSERT INTO Case_Determinations ( 'auto', 'CaseID') VALUES (9, 53);
+--our fifth case
+INSERT INTO CaseID ( 'CaseID', 'Age', 'FPL') VALUES (54, 17.75, 132);
+INSERT INTO Case_Determinations ( 'auto', 'CaseID') VALUES (10, 54);
+--our sixth case
+INSERT INTO CaseID ( 'CaseID', 'Age', 'FPL') VALUES (55, 17.5, 135);
+INSERT INTO Case_Determinations ( 'auto', 'CaseID') VALUES (11, 55);
+--our seventh case
+INSERT INTO CaseID ( 'CaseID', 'Age', 'FPL') VALUES (56, 18.5, 130);
+INSERT INTO Case_Determinations ( 'auto', 'CaseID') VALUES (12, 56);
+--our eighth case
+INSERT INTO CaseID ( 'CaseID', 'Age', 'FPL') VALUES (57, 18, 140);
+INSERT INTO Case_Determinations ( 'auto', 'CaseID') VALUES (13, 57);
+--our nineth case
+INSERT INTO CaseID ( 'CaseID', 'Age', 'FPL') VALUES (58, 68, 150);
+INSERT INTO Case_Determinations ( 'auto', 'CaseID') VALUES (14, 58);
+--our tenth case
+INSERT INTO CaseID ( 'CaseID', 'Age', 'FPL') VALUES (59, 18, 100);
+INSERT INTO Case_Determinations ( 'auto', 'CaseID') VALUES (15, 59);
+--our eleventh case
+INSERT INTO CaseID ( 'CaseID', 'Age', 'FPL') VALUES (60, 17, 120);
+INSERT INTO Case_Determinations ( 'auto', 'CaseID') VALUES (16, 60);
+-- Final wrap
+.quit
+EOF
+
+
+# Run with required case info
+./rules/MAGI-Children-HKE.py 6 41
+./rules/MAGI-Children-HKE.py 7 51
+./rules/MAGI-Children-HKE.py 8 52
+./rules/MAGI-Children-HKE.py 9 53
+./rules/MAGI-Children-HKE.py 10 54
+./rules/MAGI-Children-HKE.py 11 55
+./rules/MAGI-Children-HKE.py 12 56
+./rules/MAGI-Children-HKE.py 13 57
+./rules/MAGI-Children-HKE.py 14 58
+./rules/MAGI-Children-HKE.py 15 59
+./rules/MAGI-Children-HKE.py 16 60
+# Get results to prove 
+
+{
+sqlite3 core.db <<-EOF
+.separator "\t"
+SELECT CaseID,  Result, flag FROM Case_Determinations;
+.quit
+EOF
+} #>| rules/results/MAGI-Children-HKE-result
+
+exit 0

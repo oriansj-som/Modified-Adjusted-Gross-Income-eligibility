c5d0f137f5e2f330119762995ca8ee0cbc2a758f
Jeremiah Orians <oriansj@michigan.gov>
2019-01-02 09:48:37 -0500

    Added Newborn rule

---
+++ rules/MAGI-Children-NewBorn.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import sys
+import std_case
+import std_determination
+
+def main(Applicant, determination):
+	if Applicant.Age < 1:
+		determination.result = True
+
+	if Applicant.Age >= 1:
+		determination.result = False
+		determination.flag = "Age of " + str(Applicant.Age) + " Greater than 1;"
+
+	return determination
+
+if __name__ == '__main__':
+	Applicant = std_case.load_case(sys.argv[2])
+	determination = std_determination.load_determination(sys.argv[1])
+	determination = main(Applicant, determination)
+	std_determination.commit(determination)
